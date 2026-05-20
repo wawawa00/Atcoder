fcfe247bc95ad0856a99ff5d67860c3702d100ef
wawawa00 <hayato.wt@gmail.com>
2026-05-20 13:45:18 +0900

    0519

--- abc458/mainD.py
+++ abc458/mainD.py
@@ -5,13 +5,31 @@ def solve():
 
     X = int(next(data))
     Q = int(next(data))
-    blackboard = [X]
-    for _ in range(Q):
+    tyuuou = X
+    tyuuou_minus = 0
+    tyuuou_plus = 0
+    A1 = int(next(data))
+    B1 = int(next(data))
+    if max(A1,B1) <= tyuuou:
+        tyuuou = max(A1,B1)
+        tyuuou_plus = tyuuou
+        tyuuou_minus = min(A1, B1)
+    elif min(A1,B1) >= tyuuou:
+        tyuuou = min(A1, B1)
+        tyuuou_plus = max(A1, B1)
+        tyuuou_minus = tyuuou
+    
+    for _ in range(Q-1):
         Ai = int(next(data))
         Bi = int(next(data))
-        blackboard.append(Ai)
-        blackboard.append(Bi)
-    print(blackboard)
+
+        big = max(Ai, Bi)
+        sma = min(Ai, Bi)
+        if big <= tyuuou:
+            tyuuou = tyuuou_minus
+            tyuuou_plus 
+
+
 
 if __name__ == '__main__':
     solve()
